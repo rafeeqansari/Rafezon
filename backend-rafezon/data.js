ec8b7f3265b233408fab0f8c9ea13d8242e402c9
Rafeeq Ansari <v-ansarm@microsoft.com>
2020-08-18 02:34:53 +0530

    Templates - ReactApp- Node Server - all working

---
+++ backend-rafezon/data.js
@@ -0,0 +1,85 @@
+export const data = {
+  products: [
+    {
+      _id: "1",
+      name: 'Slim Shirt',
+      description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti repudiandae dolorum dolorem in nihil dignissimos ut aspernatur nobis doloremque qui rem, illum saepe iste delectus fugiat numquam laboriosam similique natus!",
+      catagory: 'Shirts',
+      image: "../images/d1.jpg",
+      price: 60,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: "2",
+      name: 'Fit Shirt',
+      catagory: 'Shirts',
+      image: "../images/d1.jpg",
+      price: 50,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: "3",
+      name: 'Slim Pant',
+      catagory: 'Pants',
+      image: "../images/d1.jpg",
+      price: 100,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: "4",
+      name: 'Fit Pant',
+      catagory: 'Pants',
+      image: "../images/d1.jpg",
+      price: 610,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: "5",
+      name: 'Slim Shirt',
+      catagory: 'Shirts',
+      image: "../images/d1.jpg",
+      price: 620,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: "6",
+      name: 'Slim Shirt',
+      catagory: 'Shirts',
+      image: "../images/d1.jpg",
+      price: 60,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: "7",
+      name: 'Slim Shirt',
+      catagory: 'Shirts',
+      image: "../images/d1.jpg",
+      price: 60,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    },
+    {
+      _id: "8",
+      name: 'Slim Shirt',
+      catagory: 'Shirts',
+      image: "../images/d1.jpg",
+      price: 60,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10
+    }
+  ]
+}
